d72bc3954ef6fc59632b2a10bca8539faacae204
B-Dawg <38536180+bcdurden@users.noreply.github.com>
2023-04-13 16:38:52 -0400

    updated ssh provider

--- kube.tf
+++ kube.tf
@@ -6,14 +6,6 @@ resource "ssh_resource" "retrieve_config" {
   user        = "ubuntu"
   private_key = tls_private_key.global_key.private_key_pem
 }
-# resource "ssh_resource" "retrieve_config" {
-#   host = vsphere_virtual_machine.rke2_cp_0.default_ip_address
-#   commands = [
-#     "sudo sed \"s/127.0.0.1/${var.rke2_vip}/g\" /etc/rancher/rke2/rke2.yaml"
-#   ]
-#   user        = "ubuntu"
-#   private_key = tls_private_key.global_key.private_key_pem
-# }
 resource "local_file" "kube_config_server_yaml" {
   filename = var.kubeconfig_filename
   content  = ssh_resource.retrieve_config.result
